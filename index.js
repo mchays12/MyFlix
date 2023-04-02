29c9bb988d165e5b07b55be8449a798ade3b19f0
mchays12 <mchays12@gmail.com>
2023-04-02 19:27:17 -0400

    Updating index.js
    Added a logger, installed morgan logger, created a write stream to log in my text file

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-const express = require('express');
+const express = require('express'),
+  morgan = require('morgan'),
+  fs = require('fs'), //import built in node modules fs and path
+  path =require('path');
+
+
 const app = express();
+//create a write stream (in append mode)
+// a "log.text" file is created in the root directory
+const accessLogStream = fs.createWriteStream(path.join(+dirname, 'log.text'), {flags: 'a'})
+
+//sets up logger
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('welcome to my app!');
+});
+
+app.get('/documentation', (req, res) => {
+  res.send('this is my documentation page');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
+
+
