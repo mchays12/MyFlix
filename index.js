afdbace4e887f6950eaca83cf5b6403c0c24a7f5
mchays12 <mchays12@gmail.com>
2023-04-02 20:05:09 -0400

    Updated HTML
    Moved documentation to public folder, added top 10 movies list and created get responses for them

--- index.js
+++ index.js
@@ -9,15 +9,57 @@ const app = express();
 // a "log.text" file is created in the root directory
 const accessLogStream = fs.createWriteStream(path.join(+dirname, 'log.text'), {flags: 'a'})
 
+let topTenMovies = [
+  {
+    title: 'Hereditary'
+  },
+  {
+    title: 'Midsommar'
+  },
+  {
+    title: 'John Wick'
+  },
+  {
+    title: 'John Wick 2'
+  },
+  {
+    title: 'John Wick 3'
+  },
+  {
+    title: 'John Wick 4'
+  },
+  {
+    title: 'Jaws'
+  },
+  {
+    title: 'Star Wars: a New Hope'
+  },
+  {
+    title: 'Star Wars: Empire Strikes Back'
+  },
+  {
+    title: 'Star Wars: Return of the Jedi'
+  }
+];
+
+//GET Requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my app');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topTenMovies);
+});
+
 //sets up logger
 app.use(morgan('common'));
 
-app.get('/', (req, res) => {
-  res.send('welcome to my app!');
-});
+//automatically routes all requests for static files to the "public" folder
+app.use(express.static('public'));
 
-app.get('/documentation', (req, res) => {
-  res.send('this is my documentation page');
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send("Something is broke");
 });
 
 app.listen(8080, () => {
