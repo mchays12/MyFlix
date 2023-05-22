1d85106fc901a890eeb26989fabe779949e54b11
mchays12 <mchays12@gmail.com>
2023-05-22 13:01:59 -0400

    Updates

--- index.js
+++ index.js
@@ -4,147 +4,90 @@ const express = require('express'),
   fs = require('fs'),
   path = require('path');
   uuid = require('uuid');
- 
 
-const app = express();
-//create a write stream (in append mode)  
+const app = express(); //create a write stream (in append mode)  
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.text'), {flags: 'a'}) // a "log.text" file is created in the root directory
 
-// a "log.text" file is created in the root directory
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.text'), {flags: 'a'})
+const Movies = Models.Movie;
+const Users = Models.User;
 
-//sets up logger
-app.use(morgan('combined', {stream: accessLogStream}));
+mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+  useNewUrlParser: true, useUnifiedTopology: true
+});
+
+app.use(morgan('combined', {stream: accessLogStream})); //sets up logger
 
 app.use(bodyParser.json());
 
-let users = [
-  { 
-    id: 1,
-    Name: "john",
-    favoriteMovies: []
-  }
-
-];
-
-let movies = [ 
-  {
-    'Title': 'Hereditary',
-    'Director': {
-      'Name': 'Jim Bob'
-    },
-    'Genre': {
-      'Name': 'horror'
-    }
-  },
-  {
-    'Title': 'Midsommar',
-    'Director': {
-      'Name': 'Jim Bob'
-    },
-    'Genre': {
-      'Name': 'horror'
-    }
-  },
-  {
-    'Title': 'John Wick',
-    'Director': {
-      'Name': 'Bob Jim'
-    },
-    'Genre': {
-      'Name': 'action'
-    }
-  },
-  {
-    'Title': 'John Wick 2',
-    'Director': {
-      'Name': 'Bob Jim'
-    },
-    'Genre': {
-      'Name': 'action'
-    }
-  },
-  {
-    'Title': 'John Wick 3',
-    'Director': {
-      'Name': 'Bob Jim'
-    },
-    'Genre': {
-      'Name': 'action'
-    }
-  },
-  {
-    'Title': 'John Wick 4',
-    'Director': {
-      'Name': 'Bob Jim'
-    },
-    'Genre': {
-      'Name': 'action'
-    }
-  },
-  {
-    'Title': 'Jaws',
-    'Director': {
-      'Name': 'Stephen Spielberg'
-    },
-    'Genre': {
-      'Name': 'fantasy'
-    }
-  },
-  {
-    'Title': 'Star Wars: a New Hope',
-    'Director': {
-      'Name': 'George Lucas'
-    },
-    'Genre': {
-      'Name': 'fantasy'
-    }
-  },
-  {
-    'Title': 'Star Wars: Empire Strikes Back',
-    'Director': {
-      'Name': 'George Lucas'
-    },
-    'Genre': {
-      'Name': 'fantasy'
-    }
-  },
-  {
-    'Title': 'Star Wars: Return of the Jedi',
-    'Director': {
-      'Name': 'George Lucas'
-    },
-    'Genre': {
-      'Name': 'fantasy'
-    }
-  }
-];
-
-//CREATE
+//Add a user
+/* we'll expect JSON in this format
+{
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Bithday: Date
+}*/
 app.post('/users', (req, res) => {
-  const newUser = req.body;
-  
-  if (newUser.Name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(200).json(newUser);
-  } else {
-    res.status(400).send('New user needs name');
-  }
+  Users.findOne({ Username: req.body.Username })
+  .then((user) => {
+    if (user) {
+      return res.status(400).send(req.body.Username + 'already exists');
+    } else {
+      Users
+        .create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        })
+        .then((user) => { 
+          res.status(201).json(user) 
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+        })
+      }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
-//UPDATE
-app.put('/users/:id', (req, res) => {
-  const { id } = req.params;
-  const updatedUser = req.body;
-
-  let user = users.find(user => user.id == id);
-  
-  if (user) {
-    user.Name = updatedUser.Name;
-    res.status(200).json(user)
-  } else {
-    res.status(400).send('no new user');
-  }
+// Update a user's info, by username
+/* we'll expect JSON in this format
+{
+  Username: String,
+  (required)
+  Password: String,
+  (required)
+  Email: String,
+  (required)
+  Birthday: Date
+}*/
+app.put('.users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+  $set:
+    {
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
+    }
+  },
+  { new: true }, // This line makes sure that the update document is returned
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
 });
 
 //CREATE add movie to list of favorites
@@ -189,48 +132,96 @@ app.delete('/users/:id', (req, res) => {
   }
 });
 
-//READ
+// Get all movies
 app.get('/movies', (req, res) => {
-  res.status(200).json(movies);
+  Movies.find()
+    .then((movies) => {
+      res.status(200).json(movies)
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err)
+    });
 });
 
-//READ
+// get movie by title
 app.get('/movies/:title', (req, res) => {
-  const {title} = req.params;
-  const movie = movies.find(movie => movie.Title === title);
-  
-  if (movie) {
-    res.status(200).json(movie);
-  } else {
-    res.status(400).send('No movie found');
-  }
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      if (!movie) {
+        return res.status(404).send('Error: ' + req.params.Title + ' was not found');
+      }
+      res.status(200).json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-//READ
-app.get('/movies/genre/:genreName', (req, res) => {
-  const {genreName} = req.params;
-  const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
-  
-  if (genre) {
-    res.status(200).json(genre);
-    
-  } else {
-    res.status(400).send('No genre found');
-  }
+// get a movie by genre name
+app.get('/movies/genre/:Genre', (req, res) => {
+  Movies.find({ 'Genre.Name': req.params.Genre })
+    .then((movies) => {
+      if (movies.length == 0) {
+        return res.status(404).send('Error: No movies found with the ' + req.params.Genre + ' genre were found');
+      } else {
+          res.status(200).json(movies);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-//READ
-app.get('/movies/directors/:directorName', (req, res) => {
-  const {directorName} = req.params;
-  const director = movies.find(movie => movie.Director.Name === directorName).Director;
-  
-  if (director) {
-    res.status(200).json(director);
-  } else {
-    res.status(400).send('No director found');
-  }
+// get movies by director name
+app.get('/movies/directors/:Director', (req, res) => {
+  Movies.find({ 'Director.Name': req.params.Director })
+    .then((movies) => {
+      if (movies.length == 0) {
+        return res.status(404).send('Error: No movies found with the director ' + req.params.Director + ' were found'); 
+      } else {
+        res.status(200).json(movies);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
 });
 
+// get information about a director by name
+app.get('/movies/director_information/:Director', (req, res) => {
+  Movies.findOne({ 'Director.Name': req.params.Director })
+    .then((movies) => {
+      if (movies.length == 0) {
+        return res.status(404).send('Error: ' + req.params.Director + ' was not found'); 
+      } else {
+        res.status(200).json(movie.Director);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+});
+
+// get information about a genre by name
+app.get('/movies/genre_information/:Genre', (req, res) => {
+  Movies.findOne({ 'Genre.Name': req.params.Genre })
+    .then((movies) => {
+      if (movies.length == 0) {
+        return res.status(404).send('Error: ' + req.params.Genre + ' was not found'); 
+      } else {
+        res.status(200).json(movie.Genre);
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+});
 
 //automatically routes all requests for static files to the "public" folder
 app.use(express.static('Public'));
