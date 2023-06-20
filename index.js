07a05d7c97057b573bf49916f665ecf762bbc9a6
mchays12 <mchays12@gmail.com>
2023-06-20 18:48:55 -0400

    update index

--- index.js
+++ index.js
@@ -37,7 +37,19 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:1234', 'https://myflixappmatthew.herokuapp.com/   '];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let auth = require('./auth.js')(app);
 const passport = require('passport');
