76b67995b1c82fe5f1467d1eb87243eb02b05f1c
mchays12 <mchays12@gmail.com>
2023-07-30 14:57:45 -0400

    update index

--- index.js
+++ index.js
@@ -38,11 +38,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-let allowedOrigins = ['*', 'http://localhost:1234', 'https://myflixappmatthew.herokuapp.com/'];
+let allowedOrigins = ['http://localhost:1234', 'https://myflixappmatthew.herokuapp.com/'];
 
 app.use(cors({
   origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
+    if (!origin) {
+      return callback(null, true);
+    }
     if (allowedOrigins.indexOf(origin) === -1) {
       let message = `The CORS policy for this application doesn't allow access from origin ${origin}`;
       return callback(new Error(message), false);
