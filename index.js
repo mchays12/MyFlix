827aabcd37bc3c9302174682b7c0bd0620497519
mchays12 <mchays12@gmail.com>
2023-07-31 17:10:15 -0400

    Removed cors

--- index.js
+++ index.js
@@ -39,14 +39,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-const cors = require("cors");
+/*const cors = require("cors");
 const corsOptions = {
   origin: '*',
   credentials: true,            //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 }
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(cors(corsOptions)) // Use this after the variable declaration*/
 
 let auth = require('./auth.js')(app);
 const passport = require('passport');
